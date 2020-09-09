8557ecd5a486d0d5697688be6809d31d65ac15db
Jenni Hynynen <jenni.hynynen@itvelhot.local>
2020-09-09 09:25:08 +0300

    loppu  functio

--- main.js
+++ main.js
@@ -1,36 +1,77 @@
+function laskelämpötila() {
+    var valitseMuunnos = document.getElementById("temperature").value;
+    var lämpötila = document.getElementById("lmp").value;
+    var vastaus;
+
+    if (valitseMuunnos == "CF") {
+            f = cToF(lämpötila);
+        document.getElementById("vastaus").innerHTML`${lämpötila} celsius on ${f}`; 
+    }
+
+    if (valitseMuunnos == "FC") {
+        c = fToC(numberTemp);
+    document.getElementById("vastaus").innerHTML`${lämpötila} fahrenheit on ${c}`; 
+}
+    if (valitseMuunnos == "FK") {
+    f = fToK(numberTemp);
+    document.getElementById("vastaus").innerHTML`${lämpötila} celsius on ${f}`; 
+     }
+}
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function cToF (c){
-    var fahrenheit = Math.round((celsius * 9/5) + 32 );
+    var fahrenheit = (c * 9/5) + 32 ;
          return fahrenheit;    
 }
 
 function fToC (f){
-    var celsius = Math.round((fahrenheit - 32) * 5/9) ;
+    var celsius = (f - 32) * 5/9 ;
          return celsius;
 }
 
 function fToK (f){
-    var kelvin = Math.round((+fahrenheit + 459.67) * 5/9) ;
+    var kelvin = (+f + 459.67) * 5/9 ;
          return kelvin;
 }
 
 function kToF (k){
-    var fahrenheit = Math.round(kelvin * 9/5 - 459.67);
+    var fahrenheit = k * 9/5 - 459.67;
          return fahrenheit;
 }
 
 function kToC (k){
-    var celsius = Math.round(kelvin - 273.15);
+    var celsius = k - 273.15;
          return celsius; 
 }
 
 function cToK (c){
-    var kelvin = Math.round (+celsius + 273.15);
+    var kelvin = +c + 273.15;
          return kelvin;
 }
-console.log (cToF);
 
-var pyöristys = document.getElementById("1 desimaali");
+
+
 
 function pyöristys(value) {
-    document.getElementById("1 desimaali").innerHTML = Math.round(value);
+        let desimaali = document.querySelector('input[name="desimaali"]:checked').value;
+        return value.toFixed(desimaali);
   }
